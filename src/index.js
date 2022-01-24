e08496155bcc8d8f0d900e5ac05bac07e7aca695
tanakenDX <90129428+tanakenDX@users.noreply.github.com>
2022-01-24 21:34:39 +0900

    0124_クリックイベント作成

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  // console.log(div);
+
+  //liタグ生成
+  const li = document.createElement("li");
+  console.log(inputText);
+  li.innerText = inputText;
+  console.log(li);
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
